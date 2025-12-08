2ab28457b17fa086a94097a89bfda63e5fbb1056
9athan <163199883+9athan@users.noreply.github.com>
2025-12-08 12:42:21 -0800

    Update adder.v

--- Execute.srcs/sources_1/new/adder.v
+++ Execute.srcs/sources_1/new/adder.v
@@ -1,8 +1,5 @@
 `timescale 1ns / 1ps
-/*
-Adds two 32-bit inputs to output a 32-bit value.  Takes in npcout and s_extendout to 
-output adder_out.  
-*/
+
 module adder(
     input wire [31:0] add_in1,
     input wire [31:0] add_in2,
@@ -10,4 +7,5 @@ module adder(
     );
  
 assign add_out = add_in1 + add_in2;
+
 endmodule
