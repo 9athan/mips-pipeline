1c8b1d65e01a6356c89e1337e55a73197f06c4e0
9athan <163199883+9athan@users.noreply.github.com>
2025-12-08 12:39:33 -0800

    Update imem.v

--- Writeback.srcs/sources_1/new/imem.v
+++ Writeback.srcs/sources_1/new/imem.v
@@ -1,7 +1,5 @@
 `timescale 1ns / 1ps
 
-// imem.v  --  Instruction memory (128 x 32)
-
 module I_MEM (
     input  [31:0] Address,
     output [31:0] Instr
@@ -10,11 +8,10 @@ module I_MEM (
     reg [31:0] MEM [0:127];
 
     initial begin
-        // Use the instr.mem file given in the assignment
         $readmemb("instr.mem", MEM);
     end
 
-    // Word addressed: PC[31:2]
     assign Instr = MEM[Address[31:2]];
 
 endmodule
+
