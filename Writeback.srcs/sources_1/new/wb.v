cc1b3678debd9ac434753bcb0681a120285f6c4a
9athan <163199883+9athan@users.noreply.github.com>
2025-12-08 12:39:08 -0800

    Update wb.v

--- Writeback.srcs/sources_1/new/wb.v
+++ Writeback.srcs/sources_1/new/wb.v
@@ -1,4 +1,3 @@
-// writeback_stage.v
 
 module writeback_stage (
     input  [1:0]  mem_wb_wb,
@@ -10,16 +9,15 @@ module writeback_stage (
     output [4:0]  wb_write_reg_location,
     output [31:0] mem_wb_write_data
 );
-    // mem_wb_wb[1] = RegWrite
-    // mem_wb_wb[0] = MemToReg
+
     wire MemToReg = mem_wb_wb[0];
 
     assign wb_reg_write          = mem_wb_wb[1];
     assign wb_write_reg_location = mem_wb_write_reg;
 
     mux2 #(.WIDTH(32)) memtoreg_mux (
-        .a_true (mem_wb_read_data),   // selected when MemToReg = 1
-        .b_false(mem_wb_alu_result),  // selected when MemToReg = 0
+        .a_true (mem_wb_read_data),   
+        .b_false(mem_wb_alu_result), 
         .sel    (MemToReg),
         .y      (mem_wb_write_data)
     );
