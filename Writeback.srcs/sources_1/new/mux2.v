d29b36c7664b515e3b4f2f45c872273df4aab31a
9athan <163199883+9athan@users.noreply.github.com>
2025-12-08 12:39:20 -0800

    Update mux2.v

--- Writeback.srcs/sources_1/new/mux2.v
+++ Writeback.srcs/sources_1/new/mux2.v
@@ -1,10 +1,8 @@
 `timescale 1ns / 1ps
-// mux2.v
-// Generic 2:1 multiplexer with parameterized width
 
 module mux2 #(parameter WIDTH = 32) (
-    input  wire [WIDTH-1:0] a_true,   // selected when sel = 1
-    input  wire [WIDTH-1:0] b_false,  // selected when sel = 0
+    input  wire [WIDTH-1:0] a_true,  
+    input  wire [WIDTH-1:0] b_false, 
     input  wire             sel,
     output wire [WIDTH-1:0] y
 );
